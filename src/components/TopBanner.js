a1a21a0fd29a0824338e4e686bb5c9ea0217b75e
lmmc2 <lmmc2@cin.ufpe.br>
2018-08-10 17:46:56 -0300

    atualizando banner

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import {Image} from 'semantic-ui-react/dist/commonjs';
 import '../css/topBanner.css';
 
+const logo = require('../assets/logoCapa.svg');
+
 export default class TopBanner extends Component{
 	render(){
 		return(
       <div className="top-banner-container">
-				<div className="index-anchor" id="opei"/>
-        <Image src="https://res.cloudinary.com/dkbuneg9h/image/upload/v1524237124/opei_logo_n5taqn.svg" size="big" centered/>
+				<div className="index-anchor" id="capa"/>
+        <Image src={logo} size="big" centered/>
       </div>
 		);
 	}
