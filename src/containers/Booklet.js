c4dbf79be196bb32a637593d05b256701558d618
menezesmalu <mlmv@cin.ufpe.br>
2018-08-10 15:18:37 -0300

    add Booklets

--- src/containers/Booklet.js
+++ src/containers/Booklet.js
@@ -10,11 +10,49 @@ export default class Booklet extends Component {
 			<Navbar>
 				<div className="index-page">
 					<TopBanner/>
-					<div className="index-anchor" id="regulamento"/>
-					<Section title="REGULAMENTO">
+					<div className="index-anchor" id="word"/>
+					<Section title="Apostila Word">
 						<div style={{textAlign: "center"}}>
 							<div>
-								<p className="index-sectionText" style={{marginBottom: "40px"}}>Pedimos que as escolas, delegados e alunos inscritos leiam atentamente cada item previsto no regulamento antes da realização das provas, e entrem em <Link to="/#contato">contato</Link> conosco em caso de alguma dúvida!</p>
+								<div className="index-iframe">
+									<iframe title="notice" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1QVhCrwtcPjzaLyuCULg3g9GhtCHRS71X/preview" width="640" height="480"></iframe>
+								</div>
+							</div>
+						</div>
+					</Section>
+				</div>
+				<div className="index-page">
+					<TopBanner/>
+					<div className="index-anchor" id="ppt"/>
+					<Section title="Apostila PowerPoint">
+						<div style={{textAlign: "center"}}>
+							<div>
+								<div className="index-iframe">
+									<iframe title="notice" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1QVhCrwtcPjzaLyuCULg3g9GhtCHRS71X/preview" width="640" height="480"></iframe>
+								</div>
+							</div>
+						</div>
+					</Section>
+				</div>
+				<div className="index-page">
+					<TopBanner/>
+					<div className="index-anchor" id="excel"/>
+					<Section title="Apostila Excel">
+						<div style={{textAlign: "center"}}>
+							<div>
+								<div className="index-iframe">
+									<iframe title="notice" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1QVhCrwtcPjzaLyuCULg3g9GhtCHRS71X/preview" width="640" height="480"></iframe>
+								</div>
+							</div>
+						</div>
+					</Section>
+				</div>
+				<div className="index-page">
+					<TopBanner/>
+					<div className="index-anchor" id="gsuite"/>
+					<Section title="Apostila G Suite">
+						<div style={{textAlign: "center"}}>
+							<div>
 								<div className="index-iframe">
 									<iframe title="notice" style={{border: "none", height: "600px"}} src="https://drive.google.com/file/d/1QVhCrwtcPjzaLyuCULg3g9GhtCHRS71X/preview" width="640" height="480"></iframe>
 								</div>
